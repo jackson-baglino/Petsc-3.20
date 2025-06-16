73cfe3007c0b66dd73bf55afb7c017776319ce0f
Jackson Baglino <jbaglino@caltech.edu>
2025-06-16 14:31:28 -0700

    Remove ignored files from index

--- lib/petsc/bin/maint/petsclinter/petsclinter/util/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-# Created: Mon Jun 20 15:14:22 2022 (-0400)
-# @author: Jacob Faibussowitsch
-"""
-from ._utility import *
-from ._clang   import *
-from ._timeout import timeout
-from ._color   import Color
